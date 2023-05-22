82f0e5186c8e7ab237e964fc620b69c6898bc1ad
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-05-22 17:33:13 +0100

    Installed splash screen

--- App.tsx
+++ App.tsx
@@ -15,16 +15,52 @@ import colors from './src/theme/theme';
 import Login from './src/screens/Login/Login';
 import Home from './src/screens/Home/Home';
 import Profile from './src/screens/Profile/Profile';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+
+export type NativeStackParams = {
+  "Sign Up" : undefined;
+  Login : undefined;
+  Main: undefined;
+}
+
+export type TabStackParams = {
+  Home : undefined;
+  Profile : undefined;
+}
+
+const Tab = createBottomTabNavigator<TabStackParams>();
+const RootStack = createNativeStackNavigator<NativeStackParams>();
+
+const TabNavigationRoute = () : JSX.Element => {
+
+  return(
+    <Tab.Navigator initialRouteName='Home'>
+      <Tab.Screen name='Home' component={Home} />
+      <Tab.Screen name='Profile' component={Profile} />
+    </Tab.Navigator>
+  )
+}
 
 
 function App(): JSX.Element {
   
 
   return (
-    <View style={{flex: 1}}>
-      <StatusBar backgroundColor={colors.primary}/>
-      <Profile/>
-    </View>
+    <NavigationContainer>
+      <View style={{flex: 1}}>
+        <StatusBar backgroundColor={colors.primary}/>
+        <RootStack.Navigator screenOptions={{headerShown:false}}>
+          <RootStack.Screen name='Login' component={Login} />
+          <RootStack.Screen name='Sign Up' component={Signup} />
+          <RootStack.Screen name='Main' component={TabNavigationRoute} />
+        </RootStack.Navigator>
+      </View>
+    </NavigationContainer>
+    
   )
 }
 export default App;
